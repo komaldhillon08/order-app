c76087a8603570340cc177f6e214d49f90ae2f51
sanjay <sanjay8195850041@gmail.com>
2025-08-08 12:07:13 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import Meals from "./components/Meals.jsx";
+
+function App() {
+  return (
+    <>
+    <Header/>
+     <Meals/>
+    </>
+  );
+}
+
+export default App;
